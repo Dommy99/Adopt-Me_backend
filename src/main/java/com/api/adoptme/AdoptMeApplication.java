166a1015358c05aa85a87433fb0109c945f20118
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-05-31 15:21:51 -0500

    init

---
+++ src/main/java/com/api/adoptme/AdoptMeApplication.java
@@ -0,0 +1,13 @@
+package com.api.adoptme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/adoptme/AdoptMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.adoptme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
