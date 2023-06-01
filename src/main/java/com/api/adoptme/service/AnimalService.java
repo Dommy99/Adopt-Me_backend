d3dab01a250bdc37cbc125cfb8da9c2d6042da1b
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-06-01 16:39:55 -0500

    animal service

--- src/main/java/com/api/adoptme/service/AnimalService.java
+++ src/main/java/com/api/adoptme/service/AnimalService.java
@@ -1,4 +1,17 @@
 package com.api.adoptme.service;
 
+import com.api.adoptme.repository.AnimalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AnimalService {
+
+    private final AnimalRepository animalRepository;
+
+    @Autowired
+    public AnimalService(AnimalRepository animalRepository) {
+        this.animalRepository = animalRepository;
+    }
+
 }
