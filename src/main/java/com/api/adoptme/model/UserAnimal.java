a2c0d8481da16cca8b716ce78036c11779c97b66
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-06-01 12:13:00 -0500

    useranimal table

--- src/main/java/com/api/adoptme/model/UserAnimal.java
+++ src/main/java/com/api/adoptme/model/UserAnimal.java
@@ -17,4 +17,46 @@ public class UserAnimal {
     @ManyToOne
     @JoinColumn(name = "animal_id")
     private Animal animal;
+
+    public UserAnimal() {
+    }
+
+    public UserAnimal(Long id, User user, Animal animal) {
+        this.id = id;
+        this.user = user;
+        this.animal = animal;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+
+    @Override
+    public String toString() {
+        return "UserAnimal{" +
+                "id=" + id +
+                ", user=" + user +
+                ", animal=" + animal +
+                '}';
+    }
 }
