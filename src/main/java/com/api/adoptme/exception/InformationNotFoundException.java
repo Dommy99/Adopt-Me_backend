f9d028d7373b1e73dec588d61ed091ee19e1704d
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-06-01 16:21:22 -0500

    informationNotFoundException

--- src/main/java/com/api/adoptme/exception/InformationNotFoundException.java
+++ src/main/java/com/api/adoptme/exception/InformationNotFoundException.java
@@ -1,4 +1,16 @@
 package com.api.adoptme.exception;
 
-public class InformationNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)// sets HTTP response status to 404 Not Found
+public class InformationNotFoundException extends RuntimeException {
+    /**
+     * Constructor for InformationNotFoundException
+     *
+     * @param message the error message for the exception
+     */
+    public InformationNotFoundException(String message) {
+        super(message);
+    }
 }
