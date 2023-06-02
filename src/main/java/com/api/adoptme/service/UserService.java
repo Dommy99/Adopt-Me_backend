6abbc8856cefbc5453b7ced1a8073e4788823d7a
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-06-02 10:17:09 -0500

    createuser userservice

--- src/main/java/com/api/adoptme/service/UserService.java
+++ src/main/java/com/api/adoptme/service/UserService.java
@@ -1,8 +1,27 @@
 package com.api.adoptme.service;
 
+import com.api.adoptme.exception.InformationExistException;
 import com.api.adoptme.model.User;
+import com.api.adoptme.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class UserService {
-    public void createUser(User user1) {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User userObject) {
+        Optional<User> user = userRepository.findUserByEmail(userObject.getEmail());
+        if (user.isEmpty()) {
+            userObject.setPassword(userObject.getPassword());
+            return userRepository.save(userObject);
+        }
+        throw new InformationExistException("User with email " + userObject.getEmail() + " already exists.");
     }
 }
