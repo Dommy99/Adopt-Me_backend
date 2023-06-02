796994a6587999ca5ffa26ecb5256d853eec3e9d
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-06-02 09:15:06 -0500

    Animal repository updated

--- src/main/java/com/api/adoptme/repository/AnimalRepository.java
+++ src/main/java/com/api/adoptme/repository/AnimalRepository.java
@@ -4,6 +4,10 @@ import com.api.adoptme.model.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AnimalRepository  extends JpaRepository<Animal, Long> {
+    Optional<Animal> findByName(String name);
+    Optional<Animal> findById(Long id);
 }
