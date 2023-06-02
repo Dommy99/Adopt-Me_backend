e33bf717d6568bd7ddcae278c2635bff73655f85
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-06-01 20:52:18 -0500

    step definitions made

--- src/test/java/definitions/SpringBootCucumberTestDefinitions.java
+++ src/test/java/definitions/SpringBootCucumberTestDefinitions.java
@@ -1,4 +1,80 @@
 package definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class SpringBootCucumberTestDefinitions {
+    @Given("A email is not registered")
+    public void aEmailIsNotRegistered() {
+    }
+
+    @When("A user registers with unique email and a password")
+    public void aUserRegistersWithUniqueEmailAndAPassword() {
+        
+    }
+
+    @Then("A new user account is created and returned")
+    public void aNewUserAccountIsCreatedAndReturned() {
+        
+    }
+
+    @Given("A list of registered users")
+    public void aListOfRegisteredUsers() {
+        
+    }
+
+    @When("A registered user enters email and password")
+    public void aRegisteredUserEntersEmailAndPassword() {
+        
+    }
+
+    @Then("The user is logged into the account and provided a token")
+    public void theUserIsLoggedIntoTheAccountAndProvidedAToken() {
+        
+    }
+
+    @Given("A list of animals are available")
+    public void aListOfAnimalsAreAvailable() {
+        
+    }
+
+    @When("A user searches for all animals")
+    public void aUserSearchesForAllAnimals() {
+        
+    }
+
+    @Then("A list of all animals is returned")
+    public void aListOfAllAnimalsIsReturned() {
+        
+    }
+
+    @Given("a list of animals exists")
+    public void aListOfAnimalsExists() {
+        
+    }
+
+    @When("user adds an animal to like list")
+    public void userAddsAnAnimalToLikeList() {
+        
+    }
+
+    @Then("the animal is added to user like list")
+    public void theAnimalIsAddedToUserLikeList() {
+        
+    }
+
+    @Given("a list of liked animals exists")
+    public void aListOfLikedAnimalsExists() {
+        
+    }
+
+    @When("the animal is added to the users liked list")
+    public void theAnimalIsAddedToTheUsersLikedList() {
+        
+    }
+
+    @Then("that animals adoption information is shown")
+    public void thatAnimalsAdoptionInformationIsShown() {
+    }
 }
--- src/test/resources/features/SpringBootCucumberTestFeature.feature
+++ src/test/resources/features/SpringBootCucumberTestFeature.feature
@@ -1,1 +1,29 @@
 Feature: Adopt Me Rest API functionalities
+
+
+    # Unregistered User user stories
+  Scenario: An unregistered user is able to register
+    Given A email is not registered
+    When A user registers with unique email and a password
+    Then A new user account is created and returned
+
+  # All user login user stories
+  Scenario: A user is able to log in
+    Given A list of registered users
+    When A registered user enters email and password
+    Then The user is logged into the account and provided a token
+
+  Scenario: Any user is able to view  all animals
+    Given A list of animals are available
+    When A user searches for all animals
+    Then A list of all animals is returned
+
+  Scenario: Any logged-in user can add or remove an animal to their like list
+    Given a list of animals exists
+    When user adds an animal to like list
+    Then the animal is added to user like list
+
+  Scenario: A liked animal should return adoption agency information
+    Given a list of liked animals exists
+    When the animal is added to the users liked list
+    Then that animals adoption information is shown
