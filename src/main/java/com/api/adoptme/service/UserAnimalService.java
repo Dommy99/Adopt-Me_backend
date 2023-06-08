d2a4477adf45f97800855e61c9d9617385754a27
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-06-08 17:08:06 -0500

    updated docs removed unused code

--- src/main/java/com/api/adoptme/service/UserAnimalService.java
+++ src/main/java/com/api/adoptme/service/UserAnimalService.java
@@ -29,9 +29,10 @@ public class UserAnimalService {
     }
 
     /**
+     * Fetches the Animals that a specific User has liked.
      *
-     * @param userId
-     * @return
+     * @param userId the unique identifier of the User whose liked Animals should be retrieved.
+     * @return a List of Animal objects that the User has liked.
      */
     public List<Animal> getLikedAnimalsByUser(Long userId) {
         User user = userService.getUserById(userId);
@@ -43,9 +44,12 @@ public class UserAnimalService {
 
 
     /**
+     * Adds a specific Animal to the User's like list.
      *
-     * @param animalId
-     * @return
+     * @param animalId the unique identifier of the Animal to add to the User's like list.
+     * @return the UserAnimal object representing the User's liking of the Animal.
+     * @throws InformationExistException if the Animal is already in the User's like list.
+     * @throws AuthenticationCredentialsNotFoundException if the current User is not authenticated.
      */
     public UserAnimal addAnimalToUserLikeList(Long animalId) {
         Animal animal = animalService.getAnimalById(animalId);
@@ -69,9 +73,11 @@ public class UserAnimalService {
 
 
     /**
+     * Removes a specific Animal from the User's like list.
      *
-     * @param animalId
-     * @return
+     * @param animalId the unique identifier of the Animal to remove from the User's like list.
+     * @return the UserAnimal object representing the User's liking of the Animal that was removed.
+     * @throws InformationNotFoundException if the Animal is not in the User's like list.
      */
     public UserAnimal deleteAnimeFromUserLikelist(Long animalId) {
         Animal animal = animalService.getAnimalById(animalId);
