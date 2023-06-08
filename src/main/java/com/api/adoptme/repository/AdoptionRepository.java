cf406215c63205b58d540da6d51139fe9277a8f5
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-06-08 17:16:55 -0500

    updated code

---
+++ src/main/java/com/api/adoptme/repository/AdoptionRepository.java
@@ -0,0 +1,9 @@
+package com.api.adoptme.repository;
+
+import com.api.adoptme.model.Adoption;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdoptionRepository extends JpaRepository<Adoption, Long> {
+}
