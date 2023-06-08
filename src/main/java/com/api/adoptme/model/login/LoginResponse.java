1917236c79a383de0a3001cb35dc92d025ebb6c7
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-06-07 19:51:42 -0500

    added userid

--- src/main/java/com/api/adoptme/model/login/LoginResponse.java
+++ src/main/java/com/api/adoptme/model/login/LoginResponse.java
@@ -2,14 +2,19 @@ package com.api.adoptme.model.login;
 
 public class LoginResponse {
     private String message;
+    private Long userId; // add a new field for userId
+
     /**
      * Constructor for LoginResponse
      *
      * @param message the message to set for the response object
+     * @param userId the user id to set for the response object
      */
-    public LoginResponse(String message) {
+    public LoginResponse(String message, Long userId) {
         this.message = message;
+        this.userId = userId; // set the userId in constructor
     }
+
     /**
      * Getter for the message field
      *
@@ -18,6 +23,7 @@ public class LoginResponse {
     public String getMessage() {
         return message;
     }
+
     /**
      * Setter for the message field
      *
@@ -25,5 +31,23 @@ public class LoginResponse {
      */
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    /**
+     * Getter for the userId field
+     *
+     * @return the userId field value
+     */
+    public Long getUserId() {
+        return userId;
+    }
+
+    /**
+     * Setter for the userId field
+     *
+     * @param userId the userId to set for the response object
+     */
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 }
